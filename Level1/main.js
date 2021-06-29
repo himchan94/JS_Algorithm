618a1eac9b6ce55a8c02267557eaa42a69cad30c
healthcarefrontend <himchan9494@gmail.com>
2021-06-29 23:55:25 +0900

    20210629

--- Level1/main.js
+++ Level1/main.js
@@ -1802,3 +1802,55 @@
 // }
 
 // console.log(solution(a, b));
+
+//##################################### 배달
+
+// let a = 6;
+// let b = [
+//   [1, 2, 1],
+//   [1, 3, 2],
+//   [2, 3, 2],
+//   [3, 4, 3],
+//   [3, 5, 2],
+//   [3, 5, 3],
+//   [5, 6, 1],
+// ];
+// let c = 4;
+
+// function solution(N, road, K) {
+//   // 1.
+//   const dist = Array(N + 1).fill(Infinity);
+//   const adj = Array.from({ length: N + 1 }, () => []);
+
+//   console.log(dist);
+//   console.log(adj);
+
+//   // 1-1.
+//   road.forEach(([a, b, c]) => {
+//     adj[a].push({ to: b, time: c });
+//     adj[b].push({ to: a, time: c });
+//   });
+
+//   // 2.
+//   const pq = [{ to: 1, time: 0 }];
+//   dist[1] = 0;
+
+//   // // 3.
+//   while (pq.length) {
+//     let { to, time } = pq.pop();
+//     console.log("to", to);
+//     console.log("time", time);
+//     // 4.
+//     adj[to].forEach((next) => {
+//       if (dist[next.to] > dist[to] + next.time) {
+//         dist[next.to] = dist[to] + next.time;
+//         pq.push(next);
+//       }
+//     });
+//   }
+
+//   // 5.
+//   return dist.filter((item) => item <= K).length;
+// }
+
+// console.log(solution(a, b, c));
