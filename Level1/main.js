f7f844a88e9fc32ae8495e23a61c69f80e035983
healthcarefrontend <himchan9494@gmail.com>
2021-07-01 23:20:24 +0900

    20210701

--- Level1/main.js
+++ Level1/main.js
@@ -1932,3 +1932,21 @@
 // }
 
 // solution(a);
+
+//#################################### 위장
+
+// function solution(clothes) {
+//   var answer = 1;
+//   var obj = {};
+//   for (let i = 0; i < clothes.length; i++) {
+//     obj[clothes[i][1]] = (obj[clothes[i][1]] || 1) + 1;
+//   }
+//   // console.log(obj);
+
+//   for (let key in obj) {
+//     // console.log(key);
+//     answer *= obj[key];
+//   }
+
+//   return answer - 1;
+// }
