a0ed09cb7ce2e03e63fa218f85b1d74d7581fe2d
healthcarefrontend <himchan9494@gmail.com>
2021-07-05 23:02:44 +0900

    20210705

--- Level1/main.js
+++ Level1/main.js
@@ -2003,3 +2003,23 @@
 // let test = 7;
 // console.log(test.toString(2));
 // console.log(findBit("0111"));
+
+//########################################삼각달팽이
+
+// function solution(n) {
+//   const answer = new Array(n).fill().map((_, i) => new Array(i + 1));
+
+//   let count = 0;
+//   let x = -1;
+//   let y = 0;
+//   while (n > 0) {
+//     for (let i = 0; i < n; i++) answer[++x][y] = ++count;
+//     for (let i = 0; i < n - 1; i++) answer[x][++y] = ++count;
+//     for (let i = 0; i < n - 2; i++) answer[--x][--y] = ++count;
+//     n -= 3;
+//   }
+
+//   return answer.flatMap((e) => e);
+// }
+
+// console.log(solution(4));
