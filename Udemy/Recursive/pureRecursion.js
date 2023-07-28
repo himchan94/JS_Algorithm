c46710b37435b55f16edf57d38f13d4441576425
chimpchan94 <himchan9494@gmail.com>
2023-07-28 20:10:08 +0900

    feat: pure recursive

---
+++ Udemy/Recursive/pureRecursion.js
@@ -0,0 +1,13 @@
+function collectOddValues(arr) {
+  let newArr = [];
+
+  if (arr.length === 0) return;
+
+  if (arr[0] % 2 !== 0) {
+    newArr.push(arr[0])
+  }
+
+  newArr = newArr.concat(collectOddValues(arr.slice(1)));
+
+  return newArr;
+}
