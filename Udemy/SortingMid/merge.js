c46509d27e7ba61ef50551268aa71171113944d8
chimpchan94 <himchan9494@gmail.com>
2023-08-08 23:49:06 +0900

    feat: merging

---
+++ Udemy/SortingMid/merge.js
@@ -0,0 +1,30 @@
+// merging code
+
+function merge(arr1, arr2) {
+  let results = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
+
+merge([1, 10, 50], [2, 13, 99, 100]);
