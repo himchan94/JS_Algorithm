de500474edb33b25c8b9effecfff93215bcc77a0
himchan94 <himchan9494@gmail.com>
2023-08-15 23:58:24 +0900

    feat: create linkedList push, pop

---
+++ Algorithm_Essence/LinkedList/linkedList.js
@@ -0,0 +1,62 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// bad Way
+// const first = new Node("Hi");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you");
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
+}
+
+const list = new SinglyLinkedList();
+console.log(list);
+list.push("hi");
+console.log(list);
+list.push("my");
+console.log(list);
+list.push("name");
+console.log(list);
