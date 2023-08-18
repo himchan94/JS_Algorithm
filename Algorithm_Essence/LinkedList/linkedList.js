7995c8a286dfcb7db56f449db32c73bc734866d9
himchan94 <himchan9494@gmail.com>
2023-08-18 23:46:43 +0900

    feat: Add unshift

--- Algorithm_Essence/LinkedList/linkedList.js
+++ Algorithm_Essence/LinkedList/linkedList.js
@@ -61,6 +61,20 @@ class SinglyLinkedList {
     }
     return currentHead;
   }
+
+  unshift(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 }
 
 const list = new SinglyLinkedList();
