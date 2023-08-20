e31bd0667515253ef9091c9a402e4fede7b70ad6
himchan94 <himchan9494@gmail.com>
2023-08-20 11:57:38 +0900

    feat: Linked list

--- Algorithm_Essence/LinkedList/LinkedList.md
+++ Algorithm_Essence/LinkedList/LinkedList.md
@@ -10,3 +10,21 @@
 - Popping : Removing a node from the end of the linked list
 - Shifting: Removing a new node from the beginning of the linked list
 - Unshifting: Adding a new node to the beginning of the linked list
+- Get : Retrieving a node by it's position in the linked list.
+- Set: Changing the value of a node based on it's position in the linked list.
+- Insert: Adding a node to the linked list at a specific position
+- Remove: Removing a node from the linked list at a specific position
+- Reverse: Reversing the linked list in place
+
+### Big O of singly linked lists
+
+- insertion - O(1);
+- removal - it depends... O(1) or O(N)
+- searching - O(N)
+- Access - O(N)
+
+### RECAP
+
+- Singly linked lists are an excellent alternative to arrays when insertion and eletion at the beginning are frequently required
+- Arrays contain a build in index whereas linked lists do not
+- The idea of list data structure that consists of nodes is the foundation for other data structures like Stacks and Queues
--- Algorithm_Essence/LinkedList/linkedList.js
+++ Algorithm_Essence/LinkedList/linkedList.js
@@ -86,6 +86,64 @@ class SinglyLinkedList {
     }
     return current;
   }
+
+  set(index, val) {
+    var foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+    var newNode = new Node(val);
+    var temp = prev.next;
+    var prev = this.get(index - 1);
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    var previousNode = this.get(index - 1);
+    var removed = previousNode.next();
+    previousNode.next = removed.next;
+
+    return removed;
+  }
+
+  reverse() {
+    var node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    var next;
+    var prev = null;
+
+    for (var i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
+  print() {
+    var arr = [];
+    var current = this.head;
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log(arr);
+  }
 }
 
 const list = new SinglyLinkedList();
