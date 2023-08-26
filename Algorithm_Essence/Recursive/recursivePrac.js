3a4182596ccca15d0d20a8ea08e2584d6616105f
chimpchan94 <himchan9494@gmail.com>
2023-08-26 23:58:00 +0900

    feat: palindrome

--- Algorithm_Essence/Recursive/recursivePrac.js
+++ Algorithm_Essence/Recursive/recursivePrac.js
@@ -46,3 +46,10 @@ function fibo(num) {
 
   return fibo(num - 1) + fibo(num - 2);
 }
+
+function isPalindrome(str) {
+  if (str.length === 1) return true;
+  if (str.length === 2) return str[0] === str[1];
+  if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1));
+  return false;
+}
