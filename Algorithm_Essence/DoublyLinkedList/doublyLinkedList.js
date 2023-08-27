19baeca4cf4e9ddb6049f02e178f26bbf31dfd7f
himchan94 <himchan9494@gmail.com>
2023-08-27 23:20:24 +0900

    feat: doubly linked list skeleton

---
+++ Algorithm_Essence/DoublyLinkedList/doublyLinkedList.js
@@ -0,0 +1,27 @@
+/*
+Node
+- val
+- next
+- prev
+
+DoublyLinkedList
+- head
+- tail
+- length
+*/
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
