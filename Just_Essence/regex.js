53081650dd0077aed68fd935a50b4b37189f64ee
himchan94 <himchan9494@gmail.com>
2023-08-29 01:08:47 +0900

    feat: regex prac

--- Just_Essence/regex.js
+++ Just_Essence/regex.js
@@ -36,3 +36,22 @@ const petPattern = /([a-z\s]+):\s([a-z]+),\s([a-z]+)/i; // i를 하면 uppercase
 const petsUpdated = pets.map((pet) => pet.replace(petPattern, "$3 $2 $1"));
 
 console.log(petsUpdated);
+
+// username
+
+const usernamePattern = /^[a-z][a-z0-9]{7,29}$/i; //$ end of input, ^ beginning of input
+const result = usernamePattern.test("g1aasdasdf#!#");
+console.log(result);
+
+// combination
+const num =
+  "There are a lot of phone numbers. One is 5555555555, and another is 123-123-1234. Yet another is 321.321.4321. Another is 555 555 5555. Did you know another phone number is 1 555 555 5555 and my friend has a number of (555) 123 1234. My other friend has a number of 555 555-5555 and another has 1.987.654.3210.";
+
+const phonePattern = /1?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})/g; ///[0-9]{3}[-.\s]?[0-9]{4}/
+
+let results = num.match(phonePattern);
+if (!results) results = [];
+
+console.log(results.map((x) => x.replace(phonePattern, `($1) $2-$3`)));
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
