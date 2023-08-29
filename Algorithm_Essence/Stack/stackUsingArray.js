3df2caa3fe40a98ac43d22cee939c5851bc6ab91
himchan94 <himchan9494@gmail.com>
2023-08-29 23:42:11 +0900

    feat: arrayStack

---
+++ Algorithm_Essence/Stack/stackUsingArray.js
@@ -0,0 +1,14 @@
+// Stack using array
+
+var stack = [];
+
+// using last
+// stack.push()
+// stack.pop()
+
+// using first
+// stack.unshift();
+// stack.shift();
+
+// 인덱스가 있는 경우, 뒤에서 작업하는 것이 시간 복잡도가 좋다.
+// 보통 배열로 스택을 사용하지 않는다(인덱스 때문에 성능에 좋지 않음)
