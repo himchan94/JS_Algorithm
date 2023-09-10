934ed3662cb69843c9b9803c3d935e537d033932
himchan94 <himchan9494@gmail.com>
2023-09-10 15:24:14 +0900

    feat: Tree

---
+++ Algorithm_Essence/Tree/Tree.md
@@ -0,0 +1,49 @@
+# Tree
+
+### Objectives
+- Define what a tree is
+- compare and contrast trees and lists
+- explain the differences between trees, binary trees, and binary search trees
+- implement operations on binary search trees
+
+### What is a tree?
+- a data structure that consists of nodes in a parent / child relationship
+
+
+### Difference btw lists
+- lists : linear
+- trees : non-linear
+
+### terms
+- root : the top node in a tree
+- child : a node directly connected to another node when moving away from the Root
+- parent :  the converse notion of a child
+- siblings : a group of nodes with the same parent.
+- leaf : a node with no children
+- edge : the connection between one noe and another
+
+### applications of trees
+- HTML DOM
+- Network routing
+- Abstract syntax tree
+- Artificial intelligence
+- Computer file systems
+
+### kinds of tree
+- Trees 
+- Binary Trees (Each node should have at most two children)
+- Binary Search Tree (Special at search)
+
+### How BSTS WORK
+- Every parent node has at most two children
+- Every node to the left of a parent node is always less than the parent
+- Every node to the right of a parent node is always greater than the parent
+
+
+### Big O of BST
+
+(AVG - Notguaranteed!)
+- insertion - O(log n)
+- searching - O(log n)
+
+
---
+++ Algorithm_Essence/Tree/tree.js
@@ -0,0 +1,66 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      var current = this.root;
+      while (true) {
+        if (value === current.value) return undefined;
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+
+  contains(value) {
+    if (this.root === null) return false;
+    var current = this.root,
+      found = false;
+
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+var tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
+tree.insert(20);
+console.log(tree.contains(99));
