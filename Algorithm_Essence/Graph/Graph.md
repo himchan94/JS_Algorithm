cddddd658b4ad96c03024932d47bbefa61684a99
himchan94 <himchan9494@gmail.com>
2023-09-20 22:54:07 +0900

    feat: graph structure

---
+++ Algorithm_Essence/Graph/Graph.md
@@ -0,0 +1,19 @@
+# Graph
+
+### What are graphs
+- a graph data sturcture consists of a finite (and possibly mutable) set of vertices or nodes or points, together with a set of unordered pairs of these vertices for an undirected graph or a set of ordered pairs for a directed graph
+
+
+### Uses for graphs
+- social networks
+- location / mapping
+- routing algorithms
+- visual hierarchy
+- file system optimizations
+- everywhere
+
+
+### Terms
+- Vertex : a node
+- Edge : connection between nodes
+- Diredcted/Undirected - directions assigned to distanced between vertices
---
+++ Algorithm_Essence/Graph/graph.js
@@ -0,0 +1,29 @@
+// undirective graph
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2);
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1);
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex]?.length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+}
